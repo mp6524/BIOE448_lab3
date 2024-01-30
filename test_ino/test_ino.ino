814c118ed5de5739d3e46643fe2f8af5774228f3
unknown <mp65@rice.edu>
2024-01-30 15:18:27 -0600

    Added first code

--- test_ino/test_ino.ino
+++ test_ino/test_ino.ino
@@ -1,11 +1,15 @@
 #include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   // put your setup code here, to run once:
-
+  lcd.begin(16,2); //Initiate the LCD in a 16x2 configuration
+  lcd.print("My name is:")
+  lcd.seCursor(0,1);
+  lcd.print("[Your name here!]")
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+  delay(50) //Wait 50 ms
 }
